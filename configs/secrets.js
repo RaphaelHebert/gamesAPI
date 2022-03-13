da29cbbc2ddf77b734c42b912730f715a22d6121
Joe Hebert <raphaelhebert18@gmail.com>
2022-03-13 11:35:36 +0700

    added config file and middleware

---
+++ configs/secrets.js
@@ -0,0 +1,5 @@
+const jwtsecret = process.env.JWT_SECRET || "@SecretForDevs098"
+
+module.exports = {
+    jwtsecret
+}
---
+++ configs/server.js
@@ -0,0 +1,7 @@
+const PORT = process.env.PORT || 8000
+const environment = process.env.environment || 'development'
+
+module.exports = {
+    PORT,
+    environment
+}
