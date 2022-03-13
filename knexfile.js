577cea2e02f3a6ffc8c63457026cf5b555eff3e8
Joe Hebert <raphaelhebert18@gmail.com>
2022-03-13 15:03:26 +0700

    changed config

--- knexfile.js
+++ knexfile.js
@@ -44,12 +44,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    debug: true,
     connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
       tableName: 'games',
       directory: './data/migrations' 
